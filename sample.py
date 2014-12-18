90ae360765ee5ddd25837cf335b9886615c61ebb
Randall Randall <randall@randallsquared.com>
2014-12-18 11:39:20 -0500

    refining costOf

--- sample.py
+++ sample.py
@@ -10,12 +10,14 @@ streams = [(0, 5, 10), (1, 3, 5), (3, 7, 12), (6, 11, 20), (14, 17, 8), (19, 24,
 
 
 def costOf(cost, start, end, streams):
+    if not streams: 
+        return (end - start) * cost
     total = 0
     pos = start
     for stream in streams:
         total += (stream[0] - pos) * cost
-        total += stream[3]
-        pos = stream[2]
+        total += stream[2]
+        pos = stream[1]
     return total
 
 
