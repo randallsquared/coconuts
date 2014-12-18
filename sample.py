2d1909ebf8dedefdf34054e5b12c4229bdbeb934
Randall Randall <randall@randallsquared.com>
2014-12-18 13:30:40 -0500

    first working version

--- sample.py
+++ sample.py
@@ -7,18 +7,30 @@ inputStreams = [(0, 5, 10), (1, 3, 5), (3, 7, 12), (6, 11, 20), (14, 17, 8), (19
 
 
 
-# a stream is (start, end, cost)
-streams = sorted(inputStreams, key = lambda x: x[0])
+# a stream is (start, end, cost); for more positions I'd probably give up and use a dict
+START, END, COST = (0, 1, 2)
+streams = sorted(inputStreams, key = lambda x: x[START])
 start = 0
-end = highestEndPositionIn(streams)
+end = sorted(streams, key = lambda x: x[END])[-1][END]
 
 
+def lowest(cost, start, end, possibleStreams):
+    current = []
+    if (end - start < 1) or not possibleStreams:
+        return current
+    for i, stream in enumerate(possibleStreams):
+        streamEnd = stream[1]
+        working = [stream] + lowest(cost, streamEnd, end, streamsAfter(streamEnd, possibleStreams))
+        print(working)
+        if costOf(cost, start, end, working) < costOf(cost, start, end, current): 
+            current = working
+    return current
 
-
-
-def highestEndPositionIn(streams):
-    endPos = 1
-    return sorted(streams, key = lambda x: x[endPos])[-1][endPos]
+def streamsAfter(endOfOldStream, streams):
+    for idx, stream in enumerate(streams):
+        if(endOfOldStream <= stream[START]):
+            return streams[idx:]
+    return []
 
 def costOf(cost, start, end, streams):
     if not streams: 
