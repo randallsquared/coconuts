ae2b705f0140dd471a43620effce5c8367ed4342
Randall Randall <randall@randallsquared.com>
2014-12-18 11:54:47 -0500

    find end of streams

--- sample.py
+++ sample.py
@@ -1,13 +1,24 @@
 
 
 
-# a stream is (start, end, cost)
-
+# we'll get these from file shortly
 defaultCost = 50
-streams = [(0, 5, 10), (1, 3, 5), (3, 7, 12), (6, 11, 20), (14, 17, 8), (19, 24, 14), (21, 22, 2)]
+inputStreams = [(0, 5, 10), (1, 3, 5), (3, 7, 12), (6, 11, 20), (14, 17, 8), (19, 24, 14), (21, 22, 2)]
 
 
 
+# a stream is (start, end, cost)
+streams = sorted(inputStreams, key = lambda x: x[0])
+start = 0
+end = highestEndPositionIn(streams)
+
+
+
+
+
+def highestEndPositionIn(streams):
+    endPos = 1
+    return sorted(streams, key = lambda x: x[endPos])[-1][endPos]
 
 def costOf(cost, start, end, streams):
     if not streams: 
