8b9198778b76b92760d0fe62403b460bd61ce995
Randall Randall <randall@randallsquared.com>
2014-12-19 00:42:41 -0500

    a little cleanup to make signatures nicer

--- sample.py
+++ sample.py
@@ -2,64 +2,52 @@
 
 import sys, itertools
 
-# we'll get these from file shortly
-defaultCost = None
-inputStreams = []
-
-# a stream is (start, end, cost); for more positions I'd probably give up and use a dict
 START, END, COST = (0, 1, 2)
 
+class Coconut:
 
+    def __init__(self, defaultCost, streams):
+        # a stream is (start, end, cost); for more positions I'd probably give up and use a dict
+        self.streams = sorted(streams, key = lambda x: x[START])
+        self.defaultCost = defaultCost
 
-# def lowest(cost, start, end, possibleStreams):
-#     current = []
-#     if (end - start < 1) or not possibleStreams:
-#         return current
-#     for stream in possibleStreams:
-#         working = [stream] + lowest(cost, stream[END], end, streamsAfter(stream[END], possibleStreams))
-#         if costOf(cost, start, end, working) < costOf(cost, start, end, current): 
-#             current = working
-#     return current
-# 
-# def streamsAfter(endOfOldStream, streams):
-#     for idx, stream in enumerate(streams):
-#         if endOfOldStream <= stream[START]:
-#             return streams[idx:]
-#     return []
+        self.start = 0
+        self.end = sorted(streams, key = lambda x: x[END])[-1][END]
+    
+    def lowest(self):
+        current = []
+        currentCost = ( self.end - self.start ) * self.defaultCost
+        for i in range(0, len(self.streams)):
+            print("starting with i: %s" % i)
+            for streamPath in itertools.combinations(self.streams, i):
+                if self.validStreamPath(streamPath):
+                    cost = self.costOf(streamPath)
+                    print("cost(%s): %s" % (str(streamPath), cost))
+                    if cost < currentCost:
+                        current = streamPath
+                        currentCost = cost
+        return current
+    
+    def validStreamPath(self, streamPath):
+        oldEnd = 0
+        for stream in streamPath:
+            if stream[START] < oldEnd:
+                return False
+            oldEnd = stream[END]
+        return True
+    
+    def costOf(self, streamPath):
+        if not streamPath: 
+            return (self.end - self.start) * self.defaultCost
+        total = 0
+        pos = self.start
+        for stream in streamPath:
+            total += (stream[START] - pos) * self.defaultCost
+            total += stream[COST]
+            pos = stream[END]
+        total += (self.end - pos) * self.defaultCost
+        return total
 
-
-def lowest(possibleStreams):
-    current = []
-    currentCost = ( end - start ) * defaultCost
-    for i in range(len(possibleStreams), 0, -1):
-        for streamPath in itertools.combinations(possibleStreams, i):
-            if validStreamPath(streamPath):
-                cost = costOf(defaultCost, start, end, streamPath)
-                print("cost(%s, %s, %s, %s): %s" % (defaultCost, start, end, str(streamPath), cost))
-                if cost < currentCost:
-                    current = streamPath
-                    currentCost = cost
-    return current
-
-def validStreamPath(streams):
-    oldEnd = 0
-    for stream in streams:
-        if stream[START] < oldEnd:
-            return False
-        oldEnd = stream[END]
-    return True
-
-def costOf(cost, start, end, streams):
-    if not streams: 
-        return (end - start) * cost
-    total = 0
-    pos = start
-    for stream in streams:
-        total += (stream[0] - pos) * cost
-        total += stream[2]
-        pos = stream[1]
-    total += (end - pos) * cost
-    return total
 
 
 if(len(sys.argv) < 2): 
@@ -72,6 +60,8 @@ except FileNotFoundError:
     print("Please supply a valid configuration file as the first parameter to this script.\n")
     exit()
 
+inputStreams = []
+defaultCost = None
 first = True
 for line in open(sys.argv[1], 'r'):
     if first:
@@ -86,12 +76,10 @@ if not defaultCost:
     print("Please supply a valid configuration file with the first line containing a cost integer.\n")
     exit()
 
-streams = sorted(inputStreams, key = lambda x: x[START])
-start = 0
-end = sorted(streams, key = lambda x: x[END])[-1][END]
+coco = Coconut(defaultCost, inputStreams)
 
-answer = lowest(streams)
-total = costOf(defaultCost, start, end, answer)
+answer = coco.lowest()
+total = coco.costOf(answer)
 
 print("The lowest cost path found had a cost of %s and jet streams %s\n" % (total, answer))
 
